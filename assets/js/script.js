6ff653d9f39dedf23aa7feb6b9ec6139479bd87c
mauriciodesign <mauricio.a.design@gmail.com>
2020-07-05 16:03:35 -0400

    Paginación

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,16 @@ const urlInfoPokemones = "https://pokeapi.co/api/v2/pokemon-species/";
 const urlImgPokemonDetail = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
 const urlImgPokemonFull = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/";
 
-// Eventos
+var navNext = document.querySelector("#next");
+var navBack = document.querySelector("#back");
 
-document.querySelector("#home").addEventListener("click", homePokemon);
+// Eventos
+document.querySelector("#home").addEventListener("click", homeLogo);
 document.querySelector("#boton_buscar_numero").addEventListener("click", buscarPokemonNumero);
 document.querySelector("#boton_buscar_tipo").addEventListener("click", buscarPokemonTipo);
+document.querySelector("#next").addEventListener("click", nextPokemon);
+document.querySelector("#back").addEventListener("click", homeLogo);
+
 
 //Funciones
 
@@ -24,10 +29,10 @@ function card(cardPokemon) {
   let typeBack = "";
   let typeIcon = "";
   let valor = $("#search_select").val();
-  $('.logo, #boton_buscar_numero').click(function() {
+  $('.logo,.back, #boton_buscar_numero').click(function() {
     $('select').val('Tipo de pokemon');
   });
-  $('.logo, #boton_buscar_tipo').click(function() {
+  $('.logo, .back, #boton_buscar_tipo').click(function() {
     $('input').val('');
   });
 
@@ -73,80 +78,92 @@ function modalPokemon(idPokemon, typeBack, namePokemon, tall, weight, stats, idP
   
   $.get(urlInfoPokemones + idPokemonModal, (dataPokemon) => {
 
-            let descriptionPokemon = dataPokemon.flavor_text_entries[26].flavor_text;
-            
-            $(".main").append(
-              `<div class="modal fade" id="exampleModalCenter${idPokemon}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                    <div class="modal-dialog modal-dialog-centered" role="document">
-                      <div class="modal-content ${typeBack}">
-                         <div class="modal-header">
-                         <h5 class="modal-title" id="exampleModalCenterTitle">#${idPokemon}</h5>
-                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                         <img src="assets/img/close.svg">
-                            </button>
-                         </div>
-                         <div class="modal-body">
-                           <div class="row">
-                               <div class="modal_info col-12 col-md-6">
-                                  <h1 class="modalName">${namePokemon}</h1>
-                                  <div class="pokemonModal col-12">
-                                      <img src="${urlImgPokemonFull + idPokemon}.png" alt="...">
-                                  </div>
-                                  <div class="iconType">
-                                      ${typeIcon}
-                                  </div>
-                                  <div class="caracterist col-12">
-                                      <p class="tall">${tall}m</p>
-                                      <p class="weight">${weight}kg</p>
-                                  </div>
-                                  <div class="description col-12">
-                                     <p class="infoPokemon">${descriptionPokemon}</p>
-                                  </div>
-                               </div>
-                               <div class="modal_grafico col-12 col-md-6">
-                                  <h1 class="modalName">Habilidades</h1>
-                                  <div class="graphyc" id="chartContainer${idPokemon}"></div>
-                               </div>
-                           </div>
-                         </div>
-            
-                      </div>
-                  </div>
-                </div>`
-            );
-              var chart = new CanvasJS.Chart("chartContainer" + idPokemon, {
-                animationEnabled: true,
-                theme: "light1", // "light1", "light2", "dark1", "dark2"
-                backgroundColor: "transparent",
-                axisY: {
-                  title: "",
-                },
-                data: [
-                  {
-                    type: "column",
-                    showInLegend: false,
-                    legendMarkerColor: "black",
-                    // legendText: "MMbbl = one million barrels",
-                    dataPoints: [
-                      { y: stats[0].base_stat, label: "Velocidad" },
-                      { y: stats[3].base_stat, label: "Defensa" },
-                      { y: stats[4].base_stat, label: "Ataque" },
-                      { y: stats[5].base_stat, label: "Puntos de vida" },
-                    ],
-                  },
-                ],
-              });
-              chart.render();
-          });
-
+    let descriptionPokemon = dataPokemon.flavor_text_entries[26].flavor_text;
+    
+    $(".main").append(
+      `<div class="modal fade" id="exampleModalCenter${idPokemon}" tabindex="-1" role="dialaria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+              <div class="modal-content ${typeBack}">
+                 <div class="modal-header">
+                 <h5 class="modal-title" id="exampleModalCenterTitle">#${idPokemon}</h5>
+                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                 <img src="assets/img/close.svg">
+                    </button>
+                 </div>
+                 <div class="modal-body">
+                   <div class="row">
+                       <div class="modal_info col-12 col-md-6">
+                          <h1 class="modalName">${namePokemon}</h1>
+                          <div class="pokemonModal col-12">
+                              <img src="${urlImgPokemonFull + idPokemon}.png" alt="...">
+                          </div>
+                          <div class="iconType">
+                              ${typeIcon}
+                          </div>
+                          <div class="caracterist col-12">
+                              <p class="tall">${tall}m</p>
+                              <p class="weight">${weight}kg</p>
+                          </div>
+                          <div class="description col-12">
+                             <p class="infoPokemon">${descriptionPokemon}</p>
+                          </div>
+                       </div>
+                       <div class="modal_grafico col-12 col-md-6">
+                          <h1 class="modalName">Habilidades</h1>
+                          <div class="graphyc" id="chartContainer${idPokemon}"></div>
+                       </div>
+                   </div>
+                 </div>
+    
+              </div>
+          </div>
+        </div>`
+    );
+      var chart = new CanvasJS.Chart("chartContainer" + idPokemon, {
+        animationEnabled: true,
+        theme: "light1", // "light1", "light2", "dark1", "dark2"
+        backgroundColor: "transparent",
+        axisY: {
+          title: "",
+        },
+        data: [
+          {
+            type: "column",
+            showInLegend: false,
+            legendMarkerColor: "black",
+            // legendText: "MMbbl = one million barrels",
+            dataPoints: [
+              { y: stats[0].base_stat, label: "Velocidad" },
+              { y: stats[3].base_stat, label: "Defensa" },
+              { y: stats[4].base_stat, label: "Ataque" },
+              { y: stats[5].base_stat, label: "Puntos de vida" },
+            ],
+          },
+        ],
+      });
+      chart.render();
+  });
 }
 
+function nextPokemon() {
+  homePokemon(Next);
+}
+
+function homeLogo() {
+  homePokemon(urlPokemones + '?offset=0&limit=52');
+  $(".main").html("");
+}
 
 homePokemon();
-// + '?offset=0&limit=900'
-function homePokemon() {
-  $(".main").html("");
-  $.get(urlPokemones + '?offset=0&limit=100', (data) => {
+
+function homePokemon(url) {
+  navNext.style.display = 'flex';
+  navBack.style.display = 'none';
+  if (!url) {
+    url = urlPokemones + '?offset=0&limit=52';
+  }
+  $.get(url, (data) => {
+    Next = data.next
     data.results.forEach((resultPokemon) => {
       $.get(resultPokemon.url, (totalPokemon) => {
         card(totalPokemon);
@@ -156,6 +173,8 @@ function homePokemon() {
 }
 
 function buscarPokemonNumero() {
+  navNext.style.display = 'none';
+  navBack.style.display = 'flex';
   let id = $("#idPokemon").val();
 
   if (id === "") {
@@ -177,6 +196,8 @@ $.get(urlTypePokemones, (type) => {
 });
 
 function buscarPokemonTipo() {
+  navNext.style.display = 'none'; 
+  navBack.style.display = 'flex'; 
   let valor = $("#search_select").val();
 
   if (valor === null) {
